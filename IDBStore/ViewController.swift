77272a7d6c0a7ec665e3498d4d5ff4989d19f124
Taavo <ipengtaoxx@gmail.com>
2015-11-28 16:59:38 +0800

    Initial Commit

---
+++ IDBStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDBStore
+//
+//  Created by tpeng on 15/11/28.
+//  Copyright © 2015年 tttpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
